cd1e691ffc5a4eb72d51d00a274be5d819b7d069
André Neves <andr@andr.com.br>
2023-08-29 21:06:11 -0300

    up

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -29,7 +29,7 @@
 <div class="container">
 
 <div class="row">
-<div class="col-lg-3 col-6">
+<div class="col">
 
 <div class="small-box bg-info">
 <div class="inner">
@@ -43,7 +43,7 @@
 </div>
 </div>
 
-<div class="col-lg-3 col-6">
+<div class="col">
 
 <div class="small-box bg-success">
 <div class="inner">
@@ -57,33 +57,6 @@
 </div>
 </div>
 
-<div class="col-lg-3 col-6">
-
-<div class="small-box bg-warning">
-<div class="inner">
-<h3>44</h3>
-<p>User Registrations</p>
-</div>
-<div class="icon">
-<i class="ion ion-person-add"></i>
-</div>
-<a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
-</div>
-</div>
-
-<div class="col-lg-3 col-6">
-
-<div class="small-box bg-danger">
-<div class="inner">
-<h3>65</h3>
-<p>Unique Visitors</p>
-</div>
-<div class="icon">
-<i class="ion ion-pie-graph"></i>
-</div>
-<a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
-</div>
-</div>
 
 </div>
 
