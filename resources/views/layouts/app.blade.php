b60900950891df1eb8ac6acaf0b1de89a9191f92
André Neves <andr@andr.com.br>
2023-09-19 21:41:45 -0300

    icon ion

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,7 @@
 		<title>Teste3 - sala de aula</title>
 		<meta charset="utf-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="http://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css" />
 		<link rel="stylesheet" href="{{ url('assets/css/main.css') }}" />
         <link rel="shortcut icon" href="{{ url('favicon.ico') }}">
 	</head>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,4 +2,10 @@
 
 @section('content')
     <p>Página inicial</p>
+    
+    <!-- exemplo de uso de icone ion -->
+    <!--
+    <i class="ion ion-bag"></i>
+    <h1 class="ion ion-bag">TESTE</h1>
+    -->
 @endsection
